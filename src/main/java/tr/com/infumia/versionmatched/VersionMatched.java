b6a509dc057e4291c7c74e28e4da83ba19605dde
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-06 18:20:17 +0300

    Update VersionMatched.java

--- src/main/java/tr/com/infumia/versionmatched/VersionMatched.java
+++ src/main/java/tr/com/infumia/versionmatched/VersionMatched.java
@@ -8,7 +8,7 @@ import tr.com.infumia.reflection.RefConstructed;
 import tr.com.infumia.reflection.cls.ClassOf;
 
 /**
- * matches classes with your server version and choose the right class for instantiating instead of you.
+ * matches classes with your server version and choose the right class for instantiating.
  *
  * @param version the version of the server, pattern must be like that XXX1_14_R1 XXX1_13_R2.
  * @param versionClasses the version classes.
